2a61a59e5ca4f31937a801bf4552b2c4e6a369b6
tracer-ics2024 <tracer@njuics.org>
2024-09-13 17:39:00 +0800

    >  compile NEMU 231250123 陈哲敏 Linux CC1-AL 6.10.9-arch1-2 #1 SMP PREEMPT_DYNAMIC Tue, 10 Sep 2024 14:37:32 +0000 x86_64 GNU/Linux  17:39:00 up  7:31,  2 users,  load average: 0.97, 1.32, 1.32

--- nemu/src/monitor/sdb/extra_cmd.cc
+++ nemu/src/monitor/sdb/extra_cmd.cc
@@ -1,39 +1,5 @@
 #include "extra_cmd.h"
 #include "parser.h"
-#include <tuple>
-#include <string_view>
-#include <optional>
-#include <charconv>
-#include <string>
-
-template<typename Arg>
-std::tuple<std::optional<Arg>> parse(std::string_view sv);
-
-template<>
-std::tuple<std::optional<int>> parse<int>(std::string_view sv) {
-    int res = 0;
-    if (auto [ptr, ec] = std::from_chars(sv.data(), sv.data() + sv.size(), res); ec == std::errc()) return std::make_tuple(res);
-    return std::make_tuple(std::nullopt);
-}
-
-template<>
-std::tuple<std::optional<double>> parse<double>(std::string_view sv) {
-    double res = 0;
-    if (auto [ptr, ec] = std::from_chars(sv.data(), sv.data() + sv.size(), res); ec == std::errc()) return std::make_tuple(res);
-    return std::make_tuple(std::nullopt);
-}
-
-template<>
-std::tuple<std::optional<std::string>> parse<std::string>(std::string_view sv) {
-    return std::string(sv);
-}
-
-template<typename Arg, typename... Rest>
-std::tuple<std::optional<Arg>, std::optional<Rest>...> parse(std::string_view sv) {
-    while (!sv.empty() && sv.front() == ' ') sv.remove_prefix(1);
-    auto spacePos = sv.find_first_of(' ');
-    return std::tuple_cat(parse<Arg>(sv.substr(0, spacePos)), parse<Rest...>(spacePos == std::string_view::npos ? std::string_view{} : sv.substr(spacePos + 1)));
-}
 
 extern "C" int cmd_si(char* args) {
     return 0;
