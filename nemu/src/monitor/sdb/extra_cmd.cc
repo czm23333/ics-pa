400cc4e6a66b0a5c6b0c4c14583449259d4fdbcf
tracer-ics2024 <tracer@njuics.org>
2024-09-13 18:24:21 +0800

    >  compile NEMU 231250123 陈哲敏 Linux CC1-AL 6.10.9-arch1-2 #1 SMP PREEMPT_DYNAMIC Tue, 10 Sep 2024 14:37:32 +0000 x86_64 GNU/Linux  18:24:21 up  8:16,  2 users,  load average: 0.95, 0.86, 0.98

--- nemu/src/monitor/sdb/extra_cmd.cc
+++ nemu/src/monitor/sdb/extra_cmd.cc
@@ -2,5 +2,6 @@
 #include "parser.h"
 
 extern "C" int cmd_si(char* args) {
+    parse<int>(std::string_view(args));
     return 0;
 }
--- nemu/src/monitor/sdb/parser.h
+++ nemu/src/monitor/sdb/parser.h
@@ -6,30 +6,40 @@
 #include <string>
 
 template<typename Arg>
-std::tuple<std::optional<Arg>> parse(std::string_view sv);
+std::tuple<std::optional<Arg>> parse_ker(std::string_view sv);
 
 template<>
-std::tuple<std::optional<int>> parse<int>(std::string_view sv) {
+std::tuple<std::optional<int>> parse_ker<int>(std::string_view sv) {
     int res = 0;
     if (auto [ptr, ec] = std::from_chars(sv.data(), sv.data() + sv.size(), res); ec == std::errc()) return std::make_tuple(res);
     return std::make_tuple(std::nullopt);
 }
 
 template<>
-std::tuple<std::optional<double>> parse<double>(std::string_view sv) {
+std::tuple<std::optional<double>> parse_ker<double>(std::string_view sv) {
     double res = 0;
     if (auto [ptr, ec] = std::from_chars(sv.data(), sv.data() + sv.size(), res); ec == std::errc()) return std::make_tuple(res);
     return std::make_tuple(std::nullopt);
 }
 
 template<>
-std::tuple<std::optional<std::string>> parse<std::string>(std::string_view sv) {
+std::tuple<std::optional<std::string>> parse_ker<std::string>(std::string_view sv) {
     return std::string(sv);
 }
 
+template<size_t unused = 0>
+std::tuple<> parse_impl(std::string_view sv) {
+    return std::make_tuple();
+}
+
 template<typename Arg, typename... Rest>
-std::tuple<std::optional<Arg>, std::optional<Rest>...> parse(std::string_view sv) {
+std::tuple<std::optional<Arg>, std::optional<Rest>...> parse_impl(std::string_view sv) {
     while (!sv.empty() && sv.front() == ' ') sv.remove_prefix(1);
     auto spacePos = sv.find_first_of(' ');
-    return std::tuple_cat(parse<Arg>(sv.substr(0, spacePos)), parse<Rest...>(spacePos == std::string_view::npos ? std::string_view{} : sv.substr(spacePos + 1)));
+    return std::tuple_cat(parse_ker<Arg>(sv.substr(0, spacePos)), parse_impl<Rest...>(spacePos == std::string_view::npos ? std::string_view{} : sv.substr(spacePos + 1)));
+}
+
+template<typename... Args>
+std::tuple<std::optional<Args>...> parse(std::string_view sv) {
+    return parse_impl<Args...>(sv);
 }
