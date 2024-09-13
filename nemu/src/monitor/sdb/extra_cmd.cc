d28d4dc539652870fb4b96ac7fe7d244ffdb1c2a
tracer-ics2024 <tracer@njuics.org>
2024-09-13 21:56:58 +0800

    >  compile NEMU 231250123 陈哲敏 Linux CC1-AL 6.10.9-arch1-2 #1 SMP PREEMPT_DYNAMIC Tue, 10 Sep 2024 14:37:32 +0000 x86_64 GNU/Linux  21:56:58 up 11:49,  2 users,  load average: 1.08, 1.60, 1.43

--- nemu/src/monitor/sdb/extra_cmd.cc
+++ nemu/src/monitor/sdb/extra_cmd.cc
@@ -2,8 +2,20 @@
 #include "parser.h"
 #include <cpu/cpu.h>
 
+#include "isa.h"
+
 extern "C" int cmd_si(char* args) {
     auto [num] = parse<int>(args);
     cpu_exec(num.value_or(1));
+    return 0;
+}
+
+extern "C" int cmd_info(char* args) {
+    auto [type] = parse<std::string>(args);
+    if (type == "r") {
+        for (size_t i = 0; i < std::size(cpu.gpr); ++i) printf("Register %lu: %u", i, cpu.gpr[i]);
+    } else if (type == "w") {
+
+    } else printf("Unknown subcommand");
     return 0;
 }
---
+++ nemu/src/monitor/sdb/watchpoint.cc
@@ -0,0 +1,1 @@
+#include "watchpoint.h"
