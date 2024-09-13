39f871898a9bc84b8d56f637e2b66f3547a8676a
tracer-ics2024 <tracer@njuics.org>
2024-09-13 22:22:43 +0800

    >  compile NEMU 231250123 陈哲敏 Linux CC1-AL 6.10.9-arch1-2 #1 SMP PREEMPT_DYNAMIC Tue, 10 Sep 2024 14:37:32 +0000 x86_64 GNU/Linux  22:22:43 up 12:15,  2 users,  load average: 1.41, 1.50, 1.50

--- nemu/src/monitor/sdb/expr.h
+++ nemu/src/monitor/sdb/expr.h
@@ -1,5 +1,35 @@
 #pragma once
+#include <string>
+#include <cpu/cpu.h>
+
+#include "isa.h"
 
 class expression {
+public:
+    virtual ~expression() = default;
+
     virtual int exec();
 };
+
+class number : public expression {
+public:
+    const int num;
+    explicit number(const int& num) : num(num) {}
+
+    int exec() override {
+        return num;
+    }
+};
+
+class reg_access : public expression {
+    public:
+    const std::string name;
+    explicit reg_access(const std::string& name) : name(name) {}
+
+    int exec() override {
+        bool tmp;
+        return isa_reg_str2val(name.c_str(), &tmp);
+    }
+};
+
+
