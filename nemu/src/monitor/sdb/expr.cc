5b4c620599cc4825a35905a4c67b4eda0193c507
tracer-ics2024 <tracer@njuics.org>
2024-09-14 11:13:38 +0800

    >  compile NEMU 231250123 陈哲敏 Linux CC1-AL 6.10.9-arch1-2 #1 SMP PREEMPT_DYNAMIC Tue, 10 Sep 2024 14:37:32 +0000 x86_64 GNU/Linux  11:13:38 up  1:21,  2 users,  load average: 2.44, 1.79, 1.57

--- nemu/src/monitor/sdb/expr.cc
+++ nemu/src/monitor/sdb/expr.cc
@@ -89,15 +89,11 @@ static const unsigned register_access_reg = [] {
     parsers[0].emplace_back([](unsigned, std::string_view sv) -> std::unique_ptr<expression> {
         if (sv.empty() || sv.front() != '$') return {};
         sv.remove_prefix(1);
-        std::string name;
-        while (!sv.empty()) {
-            name += sv.front();
-            sv.remove_prefix(1);
-            bool flag = false;
-            isa_reg_str2val(name.c_str(), &flag);
-            if (flag) return std::make_unique<register_access>(name);
-        }
-        return {};
+        std::string name = std::string(sv);
+        bool flag = false;
+        isa_reg_str2val(name.c_str(), &flag);
+        if (!flag) return {};
+        return std::make_unique<register_access>(name);
     });
     return 0;
 }();
