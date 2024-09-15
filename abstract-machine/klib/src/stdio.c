75191141a67690aef082ffee390709cb89099afc
tracer-ics2024 <tracer@njuics.org>
2024-09-15 16:01:16 +0800

    >  compile NEMU 231250123 陈哲敏 Linux CC1-AL 6.10.9-arch1-2 #1 SMP PREEMPT_DYNAMIC Tue, 10 Sep 2024 14:37:32 +0000 x86_64 GNU/Linux  16:01:16 up  4:43,  2 users,  load average: 1.38, 0.89, 0.84

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -10,12 +10,49 @@ int printf(const char *fmt, ...) {
     panic("Not implemented");
 }
 
+static char* putInt(char* out, int val) {
+    if (val < 0) {
+        *out++ = '-';
+        val = -val;
+    }
+    while (val) {
+        *out++ = '0' + val % 10;
+        val /= 10;
+    }
+    *out = '\0';
+    return out;
+}
+
 int vsprintf(char *out, const char *fmt, va_list ap) {
-    panic("Not implemented");
+    char *org = out;
+    while (*fmt) {
+        if (*fmt == '%') {
+            ++fmt;
+            if (*fmt == 0) continue;
+            switch (*fmt) {
+                case 's':
+                    char* str = va_arg(ap, char *);
+                    strcpy(out, str);
+                    out += strlen(str);
+                    break;
+                case 'd':
+                case 'i':
+                    out = putInt(out, va_arg(ap, int));
+                    break;
+                default:
+                    break;
+            }
+            ++fmt;
+        } else *out++ = *fmt++;
+    }
+    *out = '\0';
+    return out - org;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-    panic("Not implemented");
+    va_list args;
+    va_start(args, fmt);
+    return vsprintf(out, fmt, args);
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
