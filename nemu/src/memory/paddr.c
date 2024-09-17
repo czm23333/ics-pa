d22ef4ee923f9af3e9dc9ce5d5b19666560b1371
tracer-ics2024 <tracer@njuics.org>
2024-09-17 12:07:47 +0800

    >  compile NEMU 231250123 陈哲敏 Linux CC1-AL 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  12:07:47 up  1:42,  2 users,  load average: 0.86, 1.04, 0.80

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,6 +51,9 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
+#ifdef CONFIG_MTRACE_COND
+    if (MTRACE_COND) { log_write("Memory read: addr=" FMT_PADDR ", len=%d\n", addr, len); }
+#endif
     if (likely(in_pmem(addr))) return pmem_read(addr, len);
     IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
     out_of_bound(addr);
@@ -58,6 +61,9 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+#ifdef CONFIG_MTRACE_COND
+    if (MTRACE_COND) { log_write("Memory write: addr=" FMT_PADDR ", len=%d\n", addr, len); }
+#endif
     if (likely(in_pmem(addr))) {
         pmem_write(addr, len, data);
         return;
