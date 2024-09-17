7e1db18e0e70063a905a74cd5c4b300918317ca3
tracer-ics2024 <tracer@njuics.org>
2024-09-18 00:19:42 +0800

    >  compile NEMU 231250123 陈哲敏 Linux CC1-AL 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  00:19:42 up  2:24,  2 users,  load average: 1.77, 1.41, 1.23

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,18 +1,35 @@
 #include <am.h>
 #include <nemu.h>
 
+typedef union {
+    uint64_t time;
+    struct {
+        uint32_t timeHigh, timeLow;
+    };
+} time_t;
+
+time_t beginTime;
+
+static void read_time(time_t* time) {
+    time->timeLow = inl(RTC_ADDR);
+    time->timeHigh = inl(RTC_ADDR + 4);
+}
+
 void __am_timer_init() {
+    read_time(&beginTime);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+    time_t now;
+    read_time(&now);
+    uptime->us = now.time - beginTime.time;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
-  rtc->second = 0;
-  rtc->minute = 0;
-  rtc->hour   = 0;
-  rtc->day    = 0;
-  rtc->month  = 0;
-  rtc->year   = 1900;
+    rtc->second = 0;
+    rtc->minute = 0;
+    rtc->hour = 0;
+    rtc->day = 0;
+    rtc->month = 0;
+    rtc->year = 1900;
 }
