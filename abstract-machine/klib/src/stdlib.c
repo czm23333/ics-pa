bb64ddec479b74135505132233fe308adea91e6a
231250123-陈哲敏 <231250123@smail.nju.edu.cn>
2024-09-23 21:25:31 +0800

    Add alignment support for malloc

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -1,6 +1,7 @@
 #include <am.h>
 #include <klib.h>
 #include <klib-macros.h>
+#include <stdalign.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
@@ -37,11 +38,22 @@ typedef struct malloc_block_head {
 static malloc_block_head *first_block = NULL;
 static char malloc_buffer[64 * 1024 * 1024];
 
-static malloc_block_head *alloc_block(size_t size) {
+static void *aligned_address(void *addr, size_t alignment) {
+    return (void *) (((uintptr_t) addr + alignment - 1) & ~(alignment - 1));
+}
+
+static void *aligned_header_address(void* addr, size_t alignment) {
+    return aligned_address(addr + sizeof(malloc_block_head), alignment) - sizeof(malloc_block_head);
+}
+
+static malloc_block_head *alloc_block(size_t size, size_t alignment) {
+    if (alignment < sizeof(malloc_block_head)) return NULL;
+
     size_t relSize = size + sizeof(malloc_block_head);
     if (first_block == NULL) {
-        if (relSize > sizeof(malloc_buffer)) return NULL;
-        first_block = (malloc_block_head *) malloc_buffer;
+        char *aligned = aligned_header_address(malloc_buffer, alignment);
+        if (relSize > sizeof(malloc_buffer) - (aligned - malloc_buffer)) return NULL;
+        first_block = (malloc_block_head *) aligned;
         first_block->pre = NULL;
         first_block->next = NULL;
         first_block->size = size;
@@ -51,19 +63,22 @@ static malloc_block_head *alloc_block(size_t size) {
     size_t validSize = sizeof(malloc_buffer) + 1;
     malloc_block_head *pre_block = NULL, *cur_block = NULL, *next_block = NULL;
 
-    {
-        size_t curSize = (char *) first_block - malloc_buffer;
+    do {
+        char *aligned = aligned_header_address(malloc_buffer, alignment);
+        if (aligned > (char *) first_block) break;
+        size_t curSize = (char *) first_block - aligned;
         if (curSize >= relSize && curSize < validSize) {
             validSize = curSize;
             pre_block = NULL;
-            cur_block = (malloc_block_head*) malloc_buffer;
+            cur_block = (malloc_block_head *) aligned;
             next_block = first_block;
         }
-    }
+    } while (false);
 
     for (malloc_block_head *block = first_block; block != NULL; block = block->next) {
-        char *block_end = (char *) block + sizeof(malloc_block_head) + block->size;
-        char *region_end = block->next == NULL ? malloc_buffer + sizeof(malloc_buffer) : (char*) block->next;
+        char *block_end = aligned_header_address((char *) block + sizeof(malloc_block_head) + block->size, alignment);
+        char *region_end = block->next == NULL ? malloc_buffer + sizeof(malloc_buffer) : (char *) block->next;
+        if (block_end > region_end) continue;
         size_t empty = region_end - block_end;
         if (empty >= relSize && empty < validSize) {
             validSize = empty;
@@ -92,9 +107,20 @@ void free_block(malloc_block_head *block) {
 }
 
 void *malloc(size_t size) {
-    malloc_block_head *tmp = alloc_block(size);
+    malloc_block_head *tmp = alloc_block(size, alignof(max_align_t));
     if (tmp == NULL) return NULL;
-    return tmp + sizeof(malloc_block_head);
+    return (char *) tmp + sizeof(malloc_block_head);
+}
+
+static bool is_power_of_two(size_t x) {
+    return x && (x & (x - 1)) == 0;
+}
+
+void *aligned_alloc(size_t alignment, size_t size) {
+    if (!is_power_of_two(alignment)) return NULL;
+    malloc_block_head *tmp = alloc_block(size, alignment);
+    if (tmp == NULL) return NULL;
+    return (char *) tmp + sizeof(malloc_block_head);
 }
 
 void free(void *ptr) {
