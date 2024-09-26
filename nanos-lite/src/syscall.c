c49e5feb64f85c9b40f67453a35abde79db12150
tracer-ics2024 <tracer@njuics.org>
2024-09-26 18:55:39 +0800

    >  compile NEMU 231250123 陈哲敏 Linux CC1-AL 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  18:55:39 up  9:55,  2 users,  load average: 2.80, 2.09, 2.02

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -20,8 +20,20 @@ Context *do_syscall(Context *c) {
             return schedule(c);
         case SYS_yield:
             return schedule(c);
+        case SYS_open:
+            c->GPRx = fs_open((const char*) a[1], a[2], a[3]);
+            break;
+        case SYS_read:
+            c->GPRx = fs_read(a[1], (void*) a[2], a[3]);
+            break;
         case SYS_write:
             c->GPRx = fs_write(a[1], (const void*) a[2], a[3]);
+            break;
+        case SYS_close:
+            c->GPRx = fs_close(a[1]);
+            break;
+        case SYS_lseek:
+            c->GPRx = fs_lseek(a[1], a[2], a[3]);
             break;
         case SYS_brk:
             c->GPRx = mm_brk(a[1]);
