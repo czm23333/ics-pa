841a1894e19d417a09ce6a7b5b319a63ee2cca3c
tracer-ics2024 <tracer@njuics.org>
2024-09-26 19:27:12 +0800

    >  compile NEMU 231250123 陈哲敏 Linux CC1-AL 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  19:27:12 up 10:26,  2 users,  load average: 1.95, 1.71, 1.77

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -8,7 +8,7 @@ int main() {
     while (1) {
         struct timeval cur;
         gettimeofday(&cur, NULL);
-        printf("%u %u", cur.tv_sec, cur.tv_usec);
+        printf("%u\n", cur.tv_sec);
         if (cur.tv_sec > start.tv_sec || cur.tv_usec - start.tv_usec >= 500000) {
             start = cur;
             printf("Timer triggered");
