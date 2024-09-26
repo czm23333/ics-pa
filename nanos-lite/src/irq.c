ce58083543fb826375dfc028abc4dbbb96d3eef5
tracer-ics2024 <tracer@njuics.org>
2024-09-26 18:26:10 +0800

    >  compile NEMU 231250123 陈哲敏 Linux CC1-AL 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  18:26:10 up  9:25,  2 users,  load average: 2.11, 2.02, 2.00

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,13 +1,16 @@
 #include <common.h>
 
-Context* schedule(Context *prev);
-Context* do_syscall(Context *c);
+Context *schedule(Context *prev);
+
+Context *do_syscall(Context *c);
 
 static Context *do_event(Event e, Context *c) {
     switch (e.event) {
         case EVENT_YIELD:
-        case EVENT_IRQ_TIMER:
             return schedule(c);
+        case EVENT_IRQ_TIMER:
+            //return schedule(c);
+            break;
         case EVENT_SYSCALL:
             return do_syscall(c);
         default: panic("Unhandled event ID = %d", e.event);
