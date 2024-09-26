769f0e3e3c7f4d07e1d68aa8aae60af67b768ee3
tracer-ics2024 <tracer@njuics.org>
2024-09-26 10:10:11 +0800

    >  compile NEMU 231250123 陈哲敏 Linux CC1-AL 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  10:10:11 up  1:09,  2 users,  load average: 2.26, 2.00, 1.87

--- nanos-lite/include/mlist.h
+++ nanos-lite/include/mlist.h
@@ -7,59 +7,206 @@
 template<typename T>
 class mlist {
     struct node {
-        node* last = nullptr;
-        node* next = nullptr;
+        node *last = nullptr;
+        node *next = nullptr;
         T data;
 
-        explicit node(const T& data) noexcept : data(data) {}
-        explicit node(T&& data) noexcept : data(std::move(data)) {}
-        template<typename... Args>
-        explicit node(Args args...) noexcept : data(std::forward<Args>(args)...) {}
-    };
+        explicit node(const T &data) noexcept : data(data) {
+        }
 
-    class iterator {
-        node* current = nullptr;
-        public:
-        T& operator*() const noexcept {
-            if (current == nullptr) panic("out of bound");
-            return current->data;
+        explicit node(T &&data) noexcept : data(std::move(data)) {
+        }
+
+        template<typename... Args>
+        explicit node(Args... args) noexcept : data(std::forward<Args>(args)...) {
         }
     };
 
-    static void destroy_nodes(node* head) {
+    class iterator {
+        friend class mlist;
+        node *prev = nullptr;
+        node *current = nullptr;
+
+        iterator(node *prev, node *current) noexcept : prev(prev), current(current) {}
+
+    public:
+        T &operator*() const noexcept {
+            if (current == nullptr)
+                panic("out of bound");
+            return current->data;
+        }
+
+        iterator &operator++() noexcept {
+            if (current == nullptr)
+                panic("out of bound");
+            prev = current;
+            current = current->next;
+            return *this;
+        }
+
+        iterator operator++(int) noexcept {
+            if (current == nullptr)
+                panic("out of bound");
+            iterator tmp = *this;
+            prev = current;
+            current = current->next;
+            return tmp;
+        }
+
+        iterator &operator--() noexcept {
+            if (prev == nullptr)
+                panic("out of bound");
+            if (current != nullptr) prev = current->last;
+            current = prev;
+            prev = prev->last;
+            return *this;
+        }
+
+        iterator operator--(int) noexcept {
+            if (prev == nullptr)
+                panic("out of bound");
+            if (current != nullptr) prev = current->last;
+            iterator tmp = *this;
+            current = prev;
+            prev = prev->last;
+            return tmp;
+        }
+
+        bool operator==(const iterator &other) const noexcept {
+            return current == other.current;
+        }
+    };
+
+    static void destroy_nodes(node *head) {
         if (head == nullptr) return;
         destroy_nodes(head->next);
         delete head;
     }
 
-    static node* clone_nodes(node* last, const node* head) {
-        if (head == nullptr) return nullptr;
-        node* new_head = new node(head->data);
+    static std::pair<node *, node *> clone_nodes(node *last, const node *head) {
+        if (head == nullptr) return std::make_pair(nullptr, nullptr);
+        node *new_head = new node(head->data);
         new_head->last = last;
-        new_head->next = clone_nodes(new_head, head->next);
-        return new_head;
+        auto [a, b] = clone_nodes(new_head, head->next);
+        new_head->next = a;
+        return std::make_pair(new_head, b == nullptr ? new_head : b);
     }
 
-    node* head = nullptr;
-    size_t size = 0;
+    node *head = nullptr;
+    node *tail = nullptr;
+    size_t size_v = 0;
+
+    void insert_node(node* prev, node* cur, node* next) {
+        cur->last = prev;
+        cur->next = next;
+        if (prev != nullptr) prev->next = cur;
+        if (next != nullptr) next->last = cur;
+        if (next == head) head = cur;
+        if (prev == tail) tail = cur;
+        ++size_v;
+    }
+
+    void remove_node(node* cur) {
+        if (cur == nullptr) return;
+        node* prev = cur->last;
+        node* next = cur->next;
+        if (prev != nullptr) prev->next = next;
+        if (next != nullptr) next->last = prev;
+        if (cur == head) head = next;
+        if (cur == tail) tail = prev;
+        delete cur;
+        --size_v;
+    }
+
 public:
     mlist() noexcept = default;
 
-    mlist(const mlist& other) {
-        head = clone_nodes(nullptr, other.head);
-        size = other.size;
+    mlist(const mlist &other) {
+        auto [a, b] = clone_nodes(nullptr, other.head);;
+        head = a;
+        tail = b;
+        size_v = other.size_v;
     }
 
-    mlist(mlist&& other) noexcept {
+    mlist(mlist &&other) noexcept {
         head = other.head;
-        size = other.size;
+        tail = other.tail;
+        size_v = other.size_v;
         other.head = nullptr;
-        other.size = 0;
+        other.tail = nullptr;
+        other.size_v = 0;
     }
 
     ~mlist() noexcept {
         destroy_nodes(head);
     }
 
+    iterator begin() noexcept {
+        return iterator(nullptr, head);
+    }
 
+    iterator end() noexcept {
+        return iterator(tail, nullptr);
+    }
+
+    iterator insert(iterator pos, const T &data) noexcept {
+        node* new_node = new node(data);
+        insert_node(pos.prev, new_node, pos.current);
+        return iterator(pos.prev, new_node);
+    }
+
+    iterator insert(iterator pos, T &&data) noexcept {
+        node* new_node = new node(std::move(data));
+        insert_node(pos.prev, new_node, pos.current);
+        return iterator(pos.prev, new_node);
+    }
+
+    template<typename... Args>
+    iterator emplace(iterator pos, Args... args) noexcept {
+        node* new_node = new node(std::forward<Args>(args)...);
+        insert_node(pos.prev, new_node, pos.current);
+        return iterator(pos.prev, new_node);
+    }
+
+    void erase(iterator pos) noexcept {
+        remove_node(pos.current);
+    }
+
+    iterator push_back(const T& data) noexcept {
+        return insert(end(), data);
+    }
+
+    iterator push_back(T &&data) noexcept {
+        return insert(end(), std::move(data));
+    }
+
+    template<typename... Args>
+    iterator emplace_back(Args... args) noexcept {
+        return emplace(end(), std::forward<Args>(args)...);
+    }
+
+    iterator push_front(const T& data) noexcept {
+        return insert(begin(), data);
+    }
+
+    iterator push_front(T &&data) noexcept {
+        return insert(begin(), std::move(data));
+    }
+
+    template<typename... Args>
+    iterator emplace_front(Args... args) noexcept {
+        return emplace(begin(), std::forward<Args>(args)...);
+    }
+
+    void pop_back() noexcept {
+        erase(--end());
+    }
+
+    void pop_front() noexcept {
+        erase(begin());
+    }
+
+    [[nodiscard]] size_t size() const noexcept {
+        return size_v;
+    }
 };
---
+++ nanos-lite/src/fs.cpp
@@ -0,0 +1,60 @@
+#include <fs.h>
+#include <mlist.h>
+
+#include <externc.h>
+
+struct FDInfo;
+
+typedef size_t (*ReadFn)(void *arg, FDInfo *fd, void *buf, size_t len);
+
+typedef size_t (*WriteFn)(void *arg, FDInfo *fd, const void *buf, size_t len);
+
+typedef void (*SeekFn)(void *arg, FDInfo *fd, size_t offset);
+
+struct Finfo {
+    const char *name;
+    size_t size;
+    size_t disk_offset;
+};
+
+struct FDInfo {
+    int fd;
+    unsigned refcount;
+    void *arg;
+    size_t offset;
+    ReadFn read;
+    WriteFn write;
+    SeekFn seek;
+
+    FDInfo(int fd, unsigned refcount, void *arg, size_t offset, ReadFn read, WriteFn write,
+           SeekFn seek) : fd(fd), refcount(refcount),
+                          arg(arg), offset(offset), read(read), write(write), seek(seek) {
+    }
+};
+
+enum { FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB };
+
+mlist<FDInfo> fdList;
+
+size_t invalid_read(void *arg, FDInfo *fd, void *buf, size_t len) {
+    panic("should not reach here");
+    return 0;
+}
+
+size_t invalid_write(void *arg, FDInfo *fd, const void *buf, size_t len) {
+    panic("should not reach here");
+    return 0;
+}
+
+void invalid_seek(void *arg, FDInfo *fd, size_t offset) {
+    panic("should not reach here");
+}
+
+/* This is the information about all files in disk. */
+static Finfo file_table[] __attribute__((used)) = {
+#include "files.h"
+};
+
+EXTERNC void init_fs() {
+    fdList.emplace_back(FD_STDIN, 0xFFFFFFu, nullptr, 0, invalid_read, invalid_write, invalid_seek);
+}
