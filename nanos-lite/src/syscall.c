f0b96f77493a3f7cccdd9ff6cb65d9211cbc9124
tracer-ics2024 <tracer@njuics.org>
2024-09-26 16:48:54 +0800

    >  compile NEMU 231250123 陈哲敏 Linux CC1-AL 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  16:48:54 up  7:48,  2 users,  load average: 1.99, 1.69, 1.69

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,12 +8,16 @@ Context *schedule(Context *prev);
 Context *do_syscall(Context *c) {
     uintptr_t a[4];
     a[0] = c->GPR1;
+    a[1] = c->GPR2;
+    a[2] = c->GPR3;
+    a[3] = c->GPR4;
 
     Log("Syscall %d", a[0]);
 
     switch (a[0]) {
         case SYS_exit:
             current->running = false;
+            Log("Exit with %d", a[1]);
             return schedule(c);
         case SYS_yield:
             return schedule(c);
