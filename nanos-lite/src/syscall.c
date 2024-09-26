791478f4a40d53fef4b4d2909d4772c18426ac4c
tracer-ics2024 <tracer@njuics.org>
2024-09-26 16:59:50 +0800

    >  compile NEMU 231250123 陈哲敏 Linux CC1-AL 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  16:59:50 up  7:59,  2 users,  load average: 3.00, 2.50, 2.06

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,6 +13,8 @@ Context *do_syscall(Context *c) {
     a[2] = c->GPR3;
     a[3] = c->GPR4;
 
+    Log("Syscall %d", a[0]);
+
     switch (a[0]) {
         case SYS_exit:
             current->running = false;
