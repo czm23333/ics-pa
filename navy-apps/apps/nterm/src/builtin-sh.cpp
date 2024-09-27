77db66007bf6dfa217cdd330dba8f085e1b3a251
tracer-ics2024 <tracer@njuics.org>
2024-09-28 00:26:16 +0800

    >  compile NEMU 231250123 陈哲敏 Linux CC1-AL 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  00:26:16 up  9:56,  2 users,  load average: 2.42, 2.32, 2.15

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -32,7 +32,6 @@ void builtin_sh_run() {
   while (1) {
     SDL_Event ev;
     if (SDL_PollEvent(&ev)) {
-      printf("Event\n");
       if (ev.type == SDL_KEYUP || ev.type == SDL_KEYDOWN) {
         const char *res = term->keypress(handle_key(&ev));
         if (res) {
