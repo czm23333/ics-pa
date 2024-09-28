16f889c8e0f0bbdab70c81baaf153404eeeee0d6
tracer-ics2024 <tracer@njuics.org>
2024-09-29 00:26:17 +0800

    >  compile NEMU 231250123 陈哲敏 Linux CC1-AL 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  00:26:17 up  1:40,  2 users,  load average: 4.47, 3.28, 2.14

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -66,12 +66,13 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 void SDL_try_callback();
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
+    printf("hello\n");
     SDL_try_callback();
+    printf("hello2\n");
     if (x == 0 && y == 0 && w == 0 && h == 0) {
         w = s->w;
         h = s->h;
     }
-    printf("hello\n");
     SDL_LockSurface(s);
     NDL_OpenCanvas(&s->w, &s->h);
     uint32_t* buf = malloc(w * h * sizeof(uint32_t));
