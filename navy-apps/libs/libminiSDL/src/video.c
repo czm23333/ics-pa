ef7db8e6a02a7e5a7831c3e02aeb33624cde7abf
tracer-ics2024 <tracer@njuics.org>
2024-09-29 01:21:57 +0800

    >  compile NEMU 231250123 陈哲敏 Linux CC1-AL 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  01:21:57 up  2:36,  2 users,  load average: 2.21, 2.19, 2.28

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -8,7 +8,6 @@
 void SDL_try_callback();
 
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
-    SDL_try_callback();
     assert(dst && src);
     assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
     SDL_LockSurface(src);
@@ -45,7 +44,6 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
-    SDL_try_callback();
     SDL_Rect dstr;
     if (dstrect == NULL) {
         dstr.x = 0, dstr.y = 0;
