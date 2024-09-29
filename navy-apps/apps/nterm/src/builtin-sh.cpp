9301efcab69aaa98739d08685e51b7c9c3076043
tracer-ics2024 <tracer@njuics.org>
2024-09-29 15:16:15 +0800

    >  compile NEMU 231250123 陈哲敏 Linux CC1-AL 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  15:16:15 up  4:07,  2 users,  load average: 1.69, 1.81, 1.90

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -6,40 +6,56 @@
 char handle_key(SDL_Event *ev);
 
 static void sh_printf(const char *format, ...) {
-  static char buf[256] = {};
-  va_list ap;
-  va_start(ap, format);
-  int len = vsnprintf(buf, 256, format, ap);
-  va_end(ap);
-  term->write(buf, len);
+    static char buf[256] = {};
+    va_list ap;
+    va_start(ap, format);
+    int len = vsnprintf(buf, 256, format, ap);
+    va_end(ap);
+    term->write(buf, len);
 }
 
 static void sh_banner() {
-  sh_printf("Built-in Shell in NTerm (NJU Terminal)\n\n");
+    sh_printf("Built-in Shell in NTerm (NJU Terminal)\n\n");
 }
 
 static void sh_prompt() {
-  sh_printf("sh> ");
+    sh_printf("sh> ");
 }
 
 static void sh_handle_cmd(const char *cmd) {
+    auto cmd_buf = new char[strlen(cmd) + 1];
+    strcpy(cmd_buf, cmd);
+    char* fileName = strtok(cmd_buf, " ");
+    if (fileName == nullptr) {
+        delete[] cmd_buf;
+        return;
+    }
+    auto args = new char*[1024];
+    int argc = 0;
+    while ((args[argc] = strtok(nullptr, " ")) != nullptr)
+        ++argc;
+    char* envp[] = {nullptr};
+    execve(fileName, args, envp);
+
+    delete[] cmd_buf;
+    delete[] args;
 }
 
 void builtin_sh_run() {
-  sh_banner();
-  sh_prompt();
+    sh_banner();
+    sh_prompt();
 
-  while (1) {
-    SDL_Event ev;
-    if (SDL_PollEvent(&ev)) {
-      if (ev.type == SDL_KEYUP || ev.type == SDL_KEYDOWN) {
-        const char *res = term->keypress(handle_key(&ev));
-        if (res) {
-          sh_handle_cmd(res);
-          sh_prompt();
+    while (1) {
+        SDL_Event ev;
+        if (SDL_PollEvent(&ev)) {
+            if (ev.type == SDL_KEYUP || ev.type == SDL_KEYDOWN) {
+                const char *res = term->keypress(handle_key(&ev));
+                if (res) {
+                    sh_handle_cmd(res);
+                    sh_prompt();
+                }
+            }
         }
-      }
+        refresh_terminal();
     }
-    refresh_terminal();
-  }
 }
